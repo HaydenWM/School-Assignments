b342bfb6f07735c88c77b43294e729eb4eab4159
HaydenWM <100781647+HaydenWM@users.noreply.github.com>
2022-04-07 23:47:28 -0400

    Update Quiz Assigment.java

--- Quiz Assigment.java
+++ Quiz Assigment.java
@@ -24,9 +24,24 @@ import javax.swing.JSeparator;
 import javax.swing.JTextField;
 
 
-public class Quizclass {
+public class Quiz {
     public static JFrame MainFrame;
     public static JPanel MainPanel;
+  
+    //Pictures
+    public static JLabel Picture_Slide2;
+    public static JLabel Picture_Slide3;
+    public static JLabel Picture_Slide4;
+    public static JLabel Picture_Slide5;
+    public static JLabel Picture_Slide6;
+    public static JLabel Picture_Slide7;
+
+    //Answer Tab
+    public static JLabel Answer1;
+    public static JLabel Answer2;
+    public static JLabel Answer3;
+    public static JLabel Answer4;
+    public static JLabel Answer5;
 
     //Labels
     public static JLabel Title_Slide1;
@@ -41,7 +56,8 @@ public class Quizclass {
     public static JLabel Question_Slide8;
     public static JLabel Question_Slide9;
     public static JLabel Question_Slide10;
-
+    public static JLabel Score_label;
+    public static JLabel Score_label2;
 
 
     //Buttons
@@ -49,7 +65,6 @@ public class Quizclass {
     public static JButton Red_Button;
     public static JButton Green_Button;
     public static JButton End_Button;
-
 
 
     //submit buttons
@@ -82,7 +97,7 @@ public class Quizclass {
     public static JCheckBox Answer4_Slide6;
 
     //Arrays
-    public static String[] Slide4_Options = {"Pizza", "Noodle","Calzone", "Sushi"};
+    public static String[] Slide4_Options = {"Pizza", "Noodle", "Calzone", "Sushi"};
     public static String[] Slide3_Options = {"Lucas", "lucas"};
 
     //misc
@@ -92,44 +107,30 @@ public class Quizclass {
     public static boolean dd;
     public static String NAME;
     public static Object POGG;
+    public static Integer winlose;
+    private static double Score = 0;
+    
+   
+    
+    
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         GUI();
     }
 
-    	public static void GUI() {//this is where i make all of my swing stuff and just set them to visible false
-    	//pictures 
-    		BufferedImage img1 = null;
-    		try {
-    			img1 = ImageIO.read(new File("/Connor1.jpeg"));
-    		} catch (IOException e) {
-    		    e.printStackTrace();
-    		}
-    		
-    		Image dimg = img1.getScaledInstance(600, 450, 100);
-    		
-    		//Creating image icons
-    		ImageIcon image1 = new ImageIcon(dimg);
-    		
-    		//image loading screen
-    		Title3_Slide1 = new JLabel(image1);
-    		Title3_Slide1.setBounds(350,100,600,450);
-    		MainPanel.add(Title3_Slide1);
-   
-    	
-    		
+    public static void GUI() {//this is where i make all of my swing stuff and just set them to visible false
+ 
         // makes Frame thats used throughout
         MainFrame = new JFrame("Quiz");
         MainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        MainFrame.setSize(800, 500);     
-        
+        MainFrame.setSize(800, 500);
+
         // makes panel used throughout
         MainPanel = new JPanel();
         MainPanel.setBackground(Color.LIGHT_GRAY);
         MainFrame.add(MainPanel);
         MainPanel.setLayout(null);
-        
+
         //These items stay until score tab
         End_Button = new JButton("END");
         End_Button.setBounds(25, 425, 75, 20);
@@ -157,7 +158,140 @@ public class Quizclass {
         Start_Button.setVisible(false);
         MainPanel.add(Start_Button);
 
-      
+        //all of this adds picture
+        BufferedImage img1 = null;
+        try {
+            img1 = ImageIO.read(new File("src/Connor1.jpeg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Image dimg = img1.getScaledInstance(250, 200, 100);
+
+        //Creating image icons
+        ImageIcon image1 = new ImageIcon(dimg);
+
+        //Adds picture as a label
+        Title3_Slide1 = new JLabel(image1);
+        Title3_Slide1.setBounds(450, 100, 250, 200);
+        Title3_Slide1.setVisible(false);
+        MainPanel.add(Title3_Slide1);
+        
+        //pic2 
+        BufferedImage img2 = null;
+        try {
+            img2 = ImageIO.read(new File("src/ConnorThink.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Image dimg2 = img2.getScaledInstance(150, 150, 100);
+
+        //Creating image icons
+        ImageIcon image2 = new ImageIcon(dimg2);
+        
+        //Adds picture as a label
+        Picture_Slide2 = new JLabel(image2);
+        Picture_Slide2.setBounds(330, 200, 150, 150);
+        Picture_Slide2.setVisible(false);
+        MainPanel.add(Picture_Slide2);
+
+        //Pic3
+        BufferedImage img3 = null;
+        try {
+        	img3 = ImageIO.read(new File("src/ConnorThinksleft.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Image dimg3 = img3.getScaledInstance(200, 200, 200);
+
+        //Creating image icons
+        ImageIcon image3 = new ImageIcon(dimg3);
+
+        //Adds picture as a label
+        Picture_Slide3 = new JLabel(image3);
+        Picture_Slide3.setBounds(570, 150, 200, 200);
+        Picture_Slide3.setVisible(false);
+        MainPanel.add(Picture_Slide3);
+        
+        //Pic4
+        BufferedImage img4 = null;
+        try {
+            img4 = ImageIO.read(new File("src/ConnorHappy.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Image dimg4 = img4.getScaledInstance(250, 200, 100);
+
+        //Creating image icons
+        ImageIcon image4 = new ImageIcon(dimg4);
+
+        //Adds picture as a label
+        Picture_Slide4 = new JLabel(image4);
+        Picture_Slide4.setBounds(550, 150, 200, 200);
+        Picture_Slide4.setVisible(false);
+        MainPanel.add(Picture_Slide4);
+        
+        //Pic5
+        BufferedImage img5 = null;
+        try {
+            img5 = ImageIO.read(new File("src/ConnorThink.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Image dimg5 = img5.getScaledInstance(150, 150, 100);
+
+        //Creating image icons
+        ImageIcon image5 = new ImageIcon(dimg5);
+
+        //Adds picture as a label
+        Picture_Slide5 = new JLabel(image5);
+        Picture_Slide5.setBounds(330, 200, 150, 150);
+        Picture_Slide5.setVisible(false);
+        MainPanel.add(Picture_Slide5);
+        
+        //Pic6
+        BufferedImage img6 = null;
+        try {
+            img6 = ImageIO.read(new File("src/ConnorPoses.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Image dimg6 = img6.getScaledInstance(150, 150, 100);
+
+        //Creating image icons
+        ImageIcon image6 = new ImageIcon(dimg6);
+
+        //Adds picture as a label
+        Picture_Slide6 = new JLabel(image6);
+        Picture_Slide6.setBounds(330, 150, 150, 150);
+        Picture_Slide6.setVisible(false);
+        MainPanel.add(Picture_Slide6);
+        
+        //Pic6
+        BufferedImage img7 = null;
+        try {
+            img7 = ImageIO.read(new File("src/Connor1.jpeg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Image dimg7 = img7.getScaledInstance(250, 200, 100);
+
+        //Creating image icons
+        ImageIcon image7 = new ImageIcon(dimg7);
+
+        //Adds picture as a label
+        Picture_Slide7 = new JLabel(image7);
+        Picture_Slide7.setBounds(450, 100, 250, 200);
+        Picture_Slide7.setVisible(false);
+        MainPanel.add(Picture_Slide7);
+        
+
 
         //Slide 2
 
@@ -166,7 +300,7 @@ public class Quizclass {
         Question_Slide2.setBounds(25, 10, 700, 100);
         Question_Slide2.setVisible(false);
         MainPanel.add(Question_Slide2);
-
+        
         Sep_Slide2 = new JSeparator();
         Sep_Slide2.setBounds(10, 100, 700, 10);
         Sep_Slide2.setForeground(Color.black);
@@ -195,7 +329,7 @@ public class Quizclass {
         MainPanel.add(Question_Slide3);
 
         Answer_Slide3 = new JTextField();
-        Answer_Slide3.setBounds(100, 150, 500, 200);
+        Answer_Slide3.setBounds(50, 150, 500, 200);
         Answer_Slide3.setFont(new Font("Serif", Font.BOLD, 100));
         Answer_Slide3.setVisible(false);
         MainPanel.add(Answer_Slide3);
@@ -207,7 +341,7 @@ public class Quizclass {
 
         //Slide 4
         Answer_Slide4 = new JComboBox<Object>(Slide4_Options);
-        Answer_Slide4.setBounds(100, 150, 500, 200);
+        Answer_Slide4.setBounds(25, 150, 500, 200);
         Answer_Slide4.setFont(new Font("Serif", Font.BOLD, 100));
         Answer_Slide4.setVisible(false);
         MainPanel.add(Answer_Slide4);
@@ -218,7 +352,7 @@ public class Quizclass {
         Question_Slide4.setVisible(false);
         MainPanel.add(Question_Slide4);
 
-        Submit_Button2= new JButton("Sumbit");
+        Submit_Button2 = new JButton("Sumbit");
         Submit_Button2.setBounds(125, 425, 75, 20);
         Submit_Button2.setVisible(false);
         MainPanel.add(Submit_Button2);
@@ -253,7 +387,7 @@ public class Quizclass {
 
         //Slide 6
         Question_Slide6 = new JLabel("What are Connors 2 Favourite Games?");
-        Question_Slide6.setFont(new Font("Serif", Font.BOLD, 45));
+        Question_Slide6.setFont(new Font("Serif", Font.BOLD, 35));
         Question_Slide6.setBounds(25, 10, 700, 100);
         Question_Slide6.setVisible(false);
         MainPanel.add(Question_Slide6);
@@ -267,12 +401,12 @@ public class Quizclass {
         Answer2_Slide6.setBounds(500, 150, 200, 50);
         Answer2_Slide6.setVisible(false);
         MainPanel.add(Answer2_Slide6);
-        Answer2_Slide6.addActionListener((ActionEvent z) -> b  = true);
+        Answer2_Slide6.addActionListener((ActionEvent z) -> b = true);
         Answer3_Slide6 = new JCheckBox("City Skylines");
         Answer3_Slide6.setBounds(500, 250, 200, 50);
         Answer3_Slide6.setVisible(false);
         MainPanel.add(Answer3_Slide6);
-        Answer3_Slide6.addActionListener((ActionEvent x) -> a  = true);
+        Answer3_Slide6.addActionListener((ActionEvent x) -> a = true);
 
         Answer4_Slide6 = new JCheckBox("Fortnite");
         Answer4_Slide6.setBounds(100, 250, 200, 50);
@@ -284,41 +418,11 @@ public class Quizclass {
         Submit_Button4.setBounds(125, 425, 75, 20);
         Submit_Button4.setVisible(false);
         MainPanel.add(Submit_Button4);
+      
         //Slide 7
-        Question_Slide7 = new JLabel("If Connor had to choose 2, what ones would he pick?");
-        Question_Slide7.setFont(new Font("Serif", Font.BOLD, 25));
-        Question_Slide7.setBounds(25, 10, 700, 100);
-        Question_Slide7.setVisible(false);
-        MainPanel.add(Question_Slide7);
-
-        Submit_Button5 = new JButton("Sumbit");
-        Submit_Button5.setBounds(125, 425, 75, 20);
-        Submit_Button5.setVisible(false);
-        MainPanel.add(Submit_Button5);
-        //Slide 8
-        Question_Slide8 = new JLabel("Which Animals does Connor Own");
-        Question_Slide8.setFont(new Font("Serif", Font.BOLD, 45));
-        Question_Slide8.setBounds(25, 10, 700, 100);
-        Question_Slide8.setVisible(false);
-        MainPanel.add(Question_Slide8);
-
-        Submit_Button6 = new JButton("Sumbit");
-        Submit_Button6.setBounds(125, 425, 75, 20);
-        Submit_Button6.setVisible(false);
-        MainPanel.add(Submit_Button6);
-        //Slide 9
-        Question_Slide9 = new JLabel("What are Connors Pronouns?");
-        Question_Slide9.setFont(new Font("Serif", Font.BOLD, 45));
-        Question_Slide9.setBounds(25, 10, 700, 100);
-        Question_Slide9.setVisible(false);
-        MainPanel.add(Question_Slide9);
-
-        Submit_Button7 = new JButton("Sumbit");
-        Submit_Button7.setBounds(125, 425, 75, 20);
-        Submit_Button7.setVisible(false);
-        MainPanel.add(Submit_Button7);
-
-
+         
+        
+   
 
         SLIDE_ONE();
     }
@@ -341,6 +445,7 @@ public class Quizclass {
             Title3_Slide1.setVisible(false);
         });
     }
+
     public static void SLIDE_TWO() { //Buttons
 
 
@@ -348,13 +453,16 @@ public class Quizclass {
         End_Button.setVisible(true);
         Question_Slide2.setVisible(true);
         Sep_Slide2.setVisible(true);
-
+        Picture_Slide2.setVisible(true);
+        
         Red_Button.setVisible(true);
         Red_Button.addActionListener((ActionEvent b) -> {
             Question_Slide2.setVisible(false);
             Sep_Slide2.setVisible(false);
             Red_Button.setVisible(false);
             Green_Button.setVisible(false);
+            Picture_Slide2.setVisible(false);
+
             SLIDE_THREE();
 
         });
@@ -363,6 +471,7 @@ public class Quizclass {
         Green_Button.addActionListener((ActionEvent c) -> {
             Question_Slide2.setVisible(false);
             Sep_Slide2.setVisible(false);
+            Picture_Slide2.setVisible(false);
             Red_Button.setVisible(false);
             Green_Button.setVisible(false);
             SLIDE_THREE();
@@ -370,37 +479,46 @@ public class Quizclass {
         });
 
     }
+
     public static void SLIDE_THREE() { //Text
 
         //enable this slide
         Question_Slide3.setVisible(true);
         Answer_Slide3.setVisible(true);
         Submit_Button1.setVisible(true);
+        Picture_Slide3.setVisible(true);
         
         Submit_Button1.addActionListener((ActionEvent d) -> {
             Question_Slide3.setVisible(false);
             Answer_Slide3.setVisible(false);
             Submit_Button1.setVisible(false);
             End_Button.setVisible(false);
+            Picture_Slide3.setVisible(false);
+
             NAME = Answer_Slide3.getText();
             SLIDE_FOUR();
         });
     }
+
     public static void SLIDE_FOUR() {//ComboBox
         //Enable this slide
         Question_Slide4.setVisible(true);
         Answer_Slide4.setVisible(true);
         Submit_Button2.setVisible(true);
         End_Button.setVisible(true);
+        Picture_Slide4.setVisible(true);
 
         Submit_Button2.addActionListener((ActionEvent e) -> {
             Question_Slide4.setVisible(false);
             Submit_Button2.setVisible(false);
             POGG = Answer_Slide4.getSelectedItem();
             Answer_Slide4.setVisible(false);
+            Picture_Slide4.setVisible(false);
+
             SLIDE_FIVE();
         });
     }
+
     public static void SLIDE_FIVE() {//Check
 
 
@@ -408,6 +526,7 @@ public class Quizclass {
         Question_Slide5.setVisible(true);
         Answer_Slide5.setVisible(true);
         Answer2_Slide5.setVisible(true);
+        Picture_Slide5.setVisible(true);
 
 
 
@@ -416,18 +535,22 @@ public class Quizclass {
             Submit_Button3.setVisible(false);
             Answer_Slide5.setVisible(false);
             Answer2_Slide5.setVisible(false);
+            Picture_Slide5.setVisible(false);
 
 
 
             SLIDE_SIX();
         });
     }
+
     public static void SLIDE_SIX() { //Radio
         Question_Slide6.setVisible(true);
         Answer_Slide6.setVisible(true);
         Answer2_Slide6.setVisible(true);
         Answer3_Slide6.setVisible(true);
         Answer4_Slide6.setVisible(true);
+        Picture_Slide6.setVisible(true);
+
 
 
         Submit_Button4.setVisible(true);
@@ -436,6 +559,8 @@ public class Quizclass {
             Question_Slide6.setVisible(false);
             Answer_Slide6.setVisible(false);
             Answer2_Slide6.setVisible(false);
+            Picture_Slide6.setVisible(false);
+
             Submit_Button4.setVisible(false);
             Answer3_Slide6.setVisible(false);
             Answer4_Slide6.setVisible(false);
@@ -445,43 +570,98 @@ public class Quizclass {
 
 
     }
-    public static void SLIDE_SEVEN() {
-        //logic
 
-        //Q1
+    public static void SLIDE_SEVEN() {
+    	
+    	//i did this at like 2:00 am have mercy
+    	winlose = 0;
+    	
+        Picture_Slide7.setVisible(true);
+
         if (cc) { // Jer note: You don't need to compare booleans, just putting the name of the var is essentially saying if [VAR] is true
-            System.out.println("Question 1 is Correct");}
-        else {
-            System.out.println("Question 1 is Wrong");
+           
+        Answer1 = new JLabel("Question 1 Was Correct");
+        Answer1.setVisible(true);
+        Answer1.setBounds(50, 50, 500, 25);
+        MainPanel.add(Answer1);
+        Score += 1;      
+        } else {
+        	 Answer1 = new JLabel("Question 1 Was Wrong");
+             Answer1.setVisible(true);
+             Answer1.setBounds(50, 25, 500, 25);   
+             MainPanel.add(Answer1);
         }
         //Q2
         if (Objects.equals(NAME, "Lucas")) {
-            System.out.println("Question 2 is Correct");}//Not Working
+            Answer2 = new JLabel("Question 2 Was Correct");
+            Answer2.setVisible(true);
+            Answer2.setBounds(50, 100, 500, 25);
+            Score += 1;   
+            MainPanel.add(Answer2);
+        }//Not Working
         else {
-            System.out.println(NAME);
+            Answer2 = new JLabel("Question 2 Was Wrong");
+            Answer2.setVisible(true);
+            Answer2.setBounds(50, 100, 500, 25);   
+            MainPanel.add(Answer2);
+          
         }
         //Q3
         if (POGG == "Pizza") {
-            System.out.println("Question 3 is Correct");}
-        else {
-            System.out.println("Question 3 is Wrong" );
+        	 Answer3 = new JLabel("Question 3 Was Correct");
+             Answer3.setVisible(true);
+             Answer3.setBounds(50, 150, 500, 25);
+                 MainPanel.add(Answer3);
+                 Score += 1;        
+                 } else {
+        	 Answer3 = new JLabel("Question 3 Was Wrong");
+             Answer3.setVisible(true);
+             Answer3.setBounds(50, 150, 500, 25);
+                 MainPanel.add(Answer3);
         }
 
         //Q4
         if (dd) { // Jer note: see above
-            System.out.println("Question 4 is Correct");
-        }
-        else {
-            System.out.println("Question 4 is wrong");
-        }
+        	Answer4 = new JLabel("Question 4 Was Correct");
+            Answer4.setVisible(true);
+            Answer4.setBounds(50, 200, 500, 25);
+             MainPanel.add(Answer4);
+             Score += 1;
+             } else {
+        	Answer4 = new JLabel("Question 4 Was Wrong");
+            Answer4.setVisible(true);
+            Answer4.setBounds(50, 200, 500, 25);
+             MainPanel.add(Answer4);        }
 
         //Q5
         if (b && a) { // Jer note: see above
-            System.out.println("Question 5 is Correct");
+        	  Answer5 = new JLabel("Question 5 Was Correct");
+              Answer5.setVisible(true);
+              Answer5.setBounds(50, 250, 500, 25);
+               MainPanel.add(Answer5);    
+               Score += 1;        
+               } else {
+            Answer5 = new JLabel("Question 5 Was Wrong");
+            Answer5.setVisible(true);
+            Answer5.setBounds(50, 250, 500, 25);
+            MainPanel.add(Answer5);         
+             }
+        if (Score >= 4) {
+        	Score_label = new JLabel("You Win!");
+        	Score_label.setVisible(true);
+        	Score_label.setBounds(50, 300, 500, 25);
+             MainPanel.add(Score_label);
+        } else {
+        	Score_label = new JLabel("You Loose!");
+        	Score_label.setVisible(true);
+        	Score_label.setBounds(50, 300, 500, 25);
+             MainPanel.add(Score_label);
         }
-        else {
-            System.out.println("Question 5 is Wrong");
-        }
-
+        
+        Score_label = new JLabel("Your score was " + Score + "/5");
+    	Score_label.setVisible(true);
+    	Score_label.setBounds(50, 350, 500, 25);
+         MainPanel.add(Score_label);
+  
     }
 }
